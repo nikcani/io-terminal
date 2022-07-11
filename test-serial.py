0784576db040254cba8a6045dea73814df3cfbc5
Niklas Canisius <niklascanisius@me.com>
2022-07-11 11:30:16 +0200

    python serial testing

--- test-serial.py
+++ test-serial.py
@@ -1,14 +1,36 @@
 import serial
+import sys
+
+mode = "listen"
+if len(sys.argv) > 1:
+    mode = sys.argv[1]
+print(mode)
+
+print("python script started")
+
+
+def write_package(first, second, third):
+    ser.write(str.encode(first[0:15]))
+    ser.write(str.encode(second[0:15]))
+    ser.write(str.encode(third[0:15]))
+
 
 ser = serial.Serial()
-ser.port = '/dev/tty.usbserial-210'
-ser.baudrate = 115200
+ser.port = '/dev/tty.usbserial-0001'
+ser.baudrate = 9600
 ser.open()
 
-try:
-    while True:
-        # ser.write(b'1')
-        print(ser.readline())
-except KeyboardInterrupt:
-    print("finally")
-    ser.close()
+if mode == "listen":
+    try:
+        while True:
+            # ser.write(b'1')
+            print(ser.readline().strip())
+    except KeyboardInterrupt:
+        print("interrupt")
+elif mode == "led_turn_on":
+    write_package("Xled_turn_on", "", "")
+
+ser.close()
+print("python script stopped")
+
+# https://github.com/nikcani/smart-inventory/issues/4#issuecomment-1123724680
