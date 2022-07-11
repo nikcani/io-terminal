2a1d848b0590d654bc918476ddd2763baac7fdf2
Niklas Canisius <niklascanisius@me.com>
2022-07-11 21:46:08 +0200

    add debug to esp api

--- esp-api.py
+++ esp-api.py
@@ -1,5 +1,9 @@
+from time import sleep
+
 import serial
 import sys
+
+DEBUG = True
 
 mode = "listen"
 if len(sys.argv) > 1:
@@ -9,10 +13,23 @@ print(mode)
 print("python script started")
 
 
-def write_package(first, second, third):
-    ser.write(str.encode(first[0:15]))
-    ser.write(str.encode(second[0:15]))
-    ser.write(str.encode(third[0:15]))
+def write_line(string):
+    ser.write(str.encode(string))
+    if DEBUG:
+        print(str.encode(string))
+
+
+def write_package_line(string):
+    string = string.replace("#", "=")
+    string += "                "
+    write_line(string[0:16])
+
+
+def write_package(first, second="", third=""):
+    write_package_line(first)
+    write_package_line(second)
+    write_package_line(third)
+    write_line("#")
 
 
 ser = serial.Serial()
@@ -23,14 +40,23 @@ ser.open()
 if mode == "listen":
     try:
         while True:
-            # ser.write(b'1')
             print(ser.readline().strip())
     except KeyboardInterrupt:
         print("interrupt")
 elif mode == "led_turn_on":
-    write_package("Xled_turn_on", "", "")
+    write_package("led_turn_on", "", "")
+elif mode == "debug":
+    sleep(2)
+    write_package("display_print", "HELLO WORLD!", "YES           NO")
+    write_package("display_clear")
+    write_package("display_color", "255 000 000", "")
+    write_package("lock_open", "255 000 000", "")
+    write_package("lock_close", "255 000 000", "")
+    write_package("li_clear")
+    write_package("li_activate", "4", "000 255 000")
+    write_package("test", "the", "hashtag#filter")
 
 ser.close()
 print("python script stopped")
 
-# https://github.com/nikcani/smart-inventory/issues/4#issuecomment-1123724680
+# https://github.com/nikcani/smart-inventory/wiki/Technical-Docs#serial-protocol
