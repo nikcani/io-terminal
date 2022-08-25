69a500b3e7db0bfd471da1c0e30e99be9cd8e3c8
Niklas Canisius <niklascanisius@me.com>
2022-08-25 16:00:07 +0200

    testing

---
+++ pi/listen.py
@@ -0,0 +1,13 @@
+from lib.serialApi import SerialApi
+
+serialApi = SerialApi()
+serialApi.print_if_debug("python script started")
+
+try:
+    while True:
+        serialApi.listen_for_actions()
+except KeyboardInterrupt:
+    serialApi.print_if_debug("interrupt")
+
+serialApi.print_if_debug("python script stopped")
+del serialApi
