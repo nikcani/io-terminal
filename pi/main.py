464e182b55b3999dc61b50330ae6e24a1cac4cf9
Niklas Canisius <niklascanisius@me.com>
2022-08-25 15:08:40 +0200

    add main python file

---
+++ pi/main.py
@@ -0,0 +1,24 @@
+from time import sleep
+
+from lib.serialApi import SerialApi
+
+print("main started")
+
+serialApi = SerialApi()
+
+sleep(2)
+serialApi.display_print("HELLO WORLD!", "YES           NO")
+sleep(2)
+serialApi.display_clear()
+sleep(2)
+serialApi.display_color("255 000 000")
+sleep(2)
+serialApi.li_activate(4, "000 255 000")
+sleep(2)
+serialApi.li_clear()
+sleep(2)
+serialApi.lock_open()
+sleep(2)
+serialApi.lock_close()
+sleep(2)
+serialApi.display_print("test the", "hashtag#filter")
